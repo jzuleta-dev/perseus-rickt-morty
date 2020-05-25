da8d7f9e309f1b6f320eb03733e61ee74ad9bf55
José Zuleta <jose@zuleta.dev>
2020-05-25 21:49:41 +0200

    Update tests

--- src/__tests__/useCharacters.test.js
+++ src/__tests__/useCharacters.test.js
@@ -3,23 +3,29 @@ import { useCharacters } from "hooks/useCharacters";
 import CharactersContext from "context/CharactersContext";
 import React from "react";
 import { renderHook } from "@testing-library/react-hooks";
-xdescribe("useCharacter hook", () => {
-  const makeWrapper = () => ({ children }) => (
-    <CharactersContext.Provider
-      value={{ characters, info, setInfo, setCharacters }}
-    >
-      {children}
-    </CharactersContext.Provider>
-  );
-  beforeAll(() => {
-    jest.spyOn(global, "fetch").mockImplementation(() =>
-      Promise.resolve({
-        json: () =>
-          Promise.resolve({
-            defaultValue: characterListMock,
-          }),
-      })
+
+describe("useCharacter hook", () => {
+  const makeWrapper = () => ({ children }) => {
+    const characters = characterListMock.results;
+    const info = characterListMock.info;
+    const setInfo = jest.fn();
+    const setCharacters = jest.fn();
+    return (
+      <CharactersContext.Provider
+        value={{ characters, info, setInfo, setCharacters }}
+      >
+        {children}
+      </CharactersContext.Provider>
     );
+  };
+
+  beforeAll(() => {
+    const mockSuccessResponse = { characterListMock };
+    const mockJsonPromise = Promise.resolve(mockSuccessResponse);
+    const mockFetchPromise = Promise.resolve({
+      json: () => mockJsonPromise,
+    });
+    jest.spyOn(global, "fetch").mockImplementation(() => mockFetchPromise);
   });
 
   afterEach(() => {
@@ -33,10 +39,12 @@ xdescribe("useCharacter hook", () => {
   it("shold make api call to fetch data for the characters", async () => {
     const { result, waitForNextUpdate } = renderHook(
       () => useCharacters("rick"),
-      { wrapper: makeWrapper() }
+      {
+        wrapper: makeWrapper(),
+      }
     );
     await waitForNextUpdate();
     expect(fetch).toHaveBeenCalled();
-    console.log(result.current[0]);
+    expect(result.current.characters).toEqual(characterListMock.results);
   });
 });
